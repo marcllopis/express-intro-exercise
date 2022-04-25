dde28e5c5bb2e279a440a9bfed32685fb8b9e4a3
Marc <marcllopis89@gmail.com>
2022-04-25 11:26:03 +0200

    Adds a bootcamp path with a json on it

---
+++ bootcamp.js
@@ -0,0 +1,32 @@
+const bootcamp = {
+  students: [
+    {
+      name: "Moran",
+      living: "Berlin",
+    },
+    {
+      name: "Billy",
+      living: "France",
+    },
+    {
+      name: "Arne",
+      living: "Berlin",
+    },
+    {
+      name: "Kenneth",
+      living: "France",
+    },
+  ],
+  instructors: [
+    {
+      name: "Marc",
+      living: "Barcelona",
+    },
+    {
+      name: "Luke",
+      living: "Barcelona",
+    },
+  ],
+};
+
+module.exports = bootcamp;
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hellooo!");
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+const bootcamp = require("./bootcamp");
+
+app.get("/", (req, res) => {
+  console.log("hey from the backend!");
+  res.send("hey from backend");
+});
+
+app.get("/bootcamp", (req, res) => {
+  res.json(bootcamp);
+});
+
+app.listen(port, () => console.log(`server is running at port: ${port}`));
